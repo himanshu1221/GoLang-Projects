d30bb5c707f9b7ef75831dfde6697c767427d0d6
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-05-11 17:15:01 +0530

    Fix error in Updating API

--- Movie_CRUD_API/main.go
+++ Movie_CRUD_API/main.go
@@ -89,7 +89,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 			movies = append(movies[:index], movies[index+1:]...)
 			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movie.ID = strconv.Itoa(rand.Intn(10000))
+			movie.ID = params["id"]
 			movies = append(movies, movie)
 			json.NewEncoder(w).Encode(movie)
 		}
