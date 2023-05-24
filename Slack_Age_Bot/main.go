0f587af064ae5e06ea65fa97b33bf7080869365b
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-05-24 17:48:34 +0530

    final commit

--- Slack_Age_Bot/main.go
+++ Slack_Age_Bot/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
@@ -26,10 +27,30 @@ func main() {
 	if err != nil {
 		log.Fatalf("Some error occured. Err: %s", err)
 	}
+
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
 	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My year of birth is <year>", &slacker.CommandDefinition{
+		Description: "Year of birth calculator",
+		Examples:    []string{"My year of birth is 2003"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := 2023 - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
 	ctx, cancel := context.WithCancel(context.Background())
+
 	defer cancel()
+
 	err = bot.Listen(ctx)
 	if err != nil {
 		log.Fatalf("Some error occured. Err:%s", err)
